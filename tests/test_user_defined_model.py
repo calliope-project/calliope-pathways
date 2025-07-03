6c187bfbe77ae0cd5eeda438d1a9d4f0fd2b0ac0
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2025-07-03 09:15:30 +0100

    fix test

--- tests/test_user_defined_model.py
+++ tests/test_user_defined_model.py
@@ -1,15 +1,15 @@
+from pathlib import Path
+
 import calliope
 import pytest
 
 from calliope_pathways import models
+from calliope_pathways.util import src_dir_ref
 
 
 @pytest.fixture
 def load_example_model():
-    def _load_example_model(add_pathways_math):
-        model_path = (
-            calliope.examples._EXAMPLE_MODEL_DIR / "national_scale" / "model.yaml"
-        )
+    def _load_example_model(model_path: Path, add_pathways_math: bool):
         loaded = models.load(model_path, add_pathways_math=add_pathways_math)
         loaded.build()
         return loaded
@@ -25,17 +25,27 @@ def schema_defaults():
 
 
 def test_add_pathways_math(load_example_model):
-    model = load_example_model(True)
-    assert "flow_cap_bounding" in model.math.constraints.keys()
-    assert "flow_cap_new" in model.math.variables.keys()
-    assert "investsteps" in model.math.constraints["flow_out_max"]["foreach"]
+    model = load_example_model(
+        src_dir_ref("model_configs") / "national_scale" / "model.yaml", True
+    )
+    assert "flow_cap_bounding" in model.applied_math.data["constraints"].keys()
+    assert "flow_cap_new" in model.applied_math.data["variables"].keys()
+    assert (
+        "investsteps"
+        in model.applied_math.data["constraints"]["flow_out_max"]["foreach"]
+    )
 
 
 def test_load_no_pathways_math(load_example_model):
-    model = load_example_model(False)
-    assert "flow_cap_bounding" not in model.math.constraints.keys()
-    assert "flow_cap_new" not in model.math.variables.keys()
-    assert "investsteps" not in model.math.constraints["flow_out_max"]["foreach"]
+    model = load_example_model(
+        calliope.examples._EXAMPLE_MODEL_DIR / "national_scale" / "model.yaml", False
+    )
+    assert "flow_cap_bounding" not in model.applied_math.data["constraints"].keys()
+    assert "flow_cap_new" not in model.applied_math.data["variables"].keys()
+    assert (
+        "investsteps"
+        not in model.applied_math.data["constraints"]["flow_out_max"]["foreach"]
+    )
 
 
 def test_parameters_in_schema(schema_defaults):
